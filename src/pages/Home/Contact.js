b9944c5b957485310e25b03e96f758b03d6f6aec
Jithin K Satheesh <jithin@banzan.co>
2023-06-18 13:49:10 +0530

    almost done

--- src/pages/Home/Contact.js
+++ src/pages/Home/Contact.js
@@ -21,9 +21,9 @@ function Contact() {
     >
         <div className="pt-[100px] py-[50px] _bg-slate-100 backdrop-blur-sm ">
             <div className="container mx-auto">
+                        <JustAppear>
                 <div className="content ">
                     <div className="flex flex-wrap">
-                        <JustAppear>
 
                         <div className="w-full sm:w-fit bg-white rounded-xl p-[20px]">
 
@@ -37,10 +37,10 @@ function Contact() {
                             <ContactCard className="w-full sm:w-fit sm:min-w-[300px] lg:min-w-[450px]" />
                             </div>
                         </div>
-                        </JustAppear>
                         {/* <ContactAddress  className="w-full sm:ml-[30px] sm:w-fit sm:min-w-[300px] lg:min-w-[450px]"  /> */}
                     </div>
                 </div>
+                        </JustAppear>
 
             </div>
         </div>
